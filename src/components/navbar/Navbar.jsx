7bcd68cc3d4f47dfc1e2e7e209357dee163dc29e
geo <geo@email.com>
2022-11-11 23:59:58 -0500

    responsivo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,11 +8,8 @@ const Navbar = () =>{
   return (
     <nav>
       <a href="#" onClick={() => setActiveNav('#')}  className={activeNav==='#' ? 'active' : ''}> <AiOutlineHome/> </a>
-
       <a href="#about" onClick={() => setActiveNav('#about')}  className={activeNav==='#about' ? 'active' : ''}> <AiOutlineUser/></a>
-
       <a href="#skills" onClick={() => setActiveNav('#skills')}  className={activeNav==='#skills' ? 'active' : ''}> <BiBookBookmark/> </a>
-
       <a href="#contact"onClick={() => setActiveNav('#contact')}  className={activeNav==='#contact' ? 'active' : ''}> <BiMessageSquareEdit/> </a>
     </nav>
   )
